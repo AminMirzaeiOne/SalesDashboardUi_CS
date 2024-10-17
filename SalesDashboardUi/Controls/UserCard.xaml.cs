c566abe98394f11f0ecc8d6430d0c7a7f24fc391
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-17 09:39:00 +0330

    - Create IsActiveProperty Filed In UserCard.CS

--- SalesDashboardUi/Controls/UserCard.xaml.cs
+++ SalesDashboardUi/Controls/UserCard.xaml.cs
@@ -23,6 +23,7 @@ namespace SalesDashboardUi.Controls
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(UserCard));
         public static readonly DependencyProperty UpPriceProperty = DependencyProperty.Register("UpPrice", typeof(string), typeof(UserCard));
         public static readonly DependencyProperty DownPriceProperty = DependencyProperty.Register("DownPrice", typeof(string), typeof(UserCard));
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(UserCard));
 
 
         public UserCard()
